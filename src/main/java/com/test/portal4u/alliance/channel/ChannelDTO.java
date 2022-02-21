0245d90c3eca27fecd9e79509517a895fed3c52e
Jinn <72987464+Dev-Jinuk-Jo@users.noreply.github.com>
2022-02-21 17:48:00 +0900

    Feat(Alliance): Add alliance channel api
    Add alliance channel api

---
+++ src/main/java/com/test/portal4u/alliance/channel/ChannelDTO.java
@@ -0,0 +1,11 @@
+package com.test.portal4u.alliance.channel;
+
+import lombok.Data;
+
+@Data
+public class ChannelDTO {
+
+    private Long seq;
+    private String seqGroup;
+    private String name;
+}
