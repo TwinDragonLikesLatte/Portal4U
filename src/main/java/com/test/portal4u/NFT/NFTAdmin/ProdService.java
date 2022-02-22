fc11fc11ff9ef01fe48f10110b257fef8062284e
Kang Kujun <donato14@naver.com>
2022-02-23 01:14:06 +0900

    Feat(NFTAdmin): Add 50% Function

---
+++ src/main/java/com/test/portal4u/NFT/NFTAdmin/ProdService.java
@@ -0,0 +1,14 @@
+package com.test.portal4u.NFT.NFTAdmin;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface ProdService {
+
+	int add(ProdDTO dto, HttpServletRequest req);
+
+	List<NFTBoardDTO> list();
+
+	
+}
