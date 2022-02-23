e0f590c2108da8afc70d08f54774dd57b9e38978
helloppiok <pullip95@gmail.com>
2022-02-24 00:07:20 +0900

    Add(NTF) : Add delete files

---
+++ src/main/java/com/test/portal4u/NFT/NFTAction/NFTBoardDTO.java
@@ -0,0 +1,27 @@
+package com.test.portal4u.NFT.NFTAction;
+
+import lombok.Data;
+
+@Data
+public class NFTBoardDTO {
+
+	private String seq;
+	private String title;
+	private String content;
+	private String buseo;
+	private String regdate;
+	private String id;
+	private String password;
+	
+}
+
+
+//CREATE TABLE TBLNFTBOARD(
+//	    seq NUMBER PRIMARY KEY,
+//	    title VARCHAR2(100) NOT NULL ,
+//	    content VARCHAR2(1000) NOT NULL ,
+//	    buseo VARCHAR2(30) NULL ,
+//	    regdate date default SYSDATE,
+//	    id VARCHAR2(30) NOT NULL ,
+//	    password VARCHAR2(20) NOT NULL
+//	);
---
+++ src/main/java/com/test/portal4u/NFT/NFTAdmin/ProdDTO.java
@@ -0,0 +1,17 @@
+package com.test.portal4u.NFT.NFTAdmin;
+
+import lombok.Data;
+
+@Data
+public class ProdDTO {
+
+	private String prod_seq;
+	private String prod_name;
+	private String prod_desc;
+	private String prod_detail;
+	private String start_date;
+	private String orgfilename;
+	private String filename;
+	
+	
+}
