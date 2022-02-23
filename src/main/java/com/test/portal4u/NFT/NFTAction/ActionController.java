1d50665eea31b160c88aca2b9d03ce76137f9c6c
helloppiok <pullip95@gmail.com>
2022-02-24 00:20:53 +0900

    Add(NFT) : Add about NFT files

--- src/main/java/com/test/portal4u/NFT/NFTAction/ActionController.java
+++ src/main/java/com/test/portal4u/NFT/NFTAction/ActionController.java
@@ -1,22 +1,29 @@
 package com.test.portal4u.NFT.NFTAction;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ActionController {
-	
-	@RequestMapping(value = "/NFTAction/main", method = { RequestMethod.GET })
-	public String NFTAction(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
 
-		
+	@Autowired
+	private ActionService service;
+
+	@RequestMapping(value = "/NFTAction/main", method = { RequestMethod.GET })
+	public String NFTAction(HttpServletRequest req, HttpServletResponse resp, HttpSession session, Model model) {
+
+		List<NFTBoardDTO> list = service.list();
+
+		model.addAttribute("list", list);
 
 		return "NFTAction.main";
 	}
-
-}
--- src/main/java/com/test/portal4u/NFT/NFTAction/ActionServiceImpl.java
+++ src/main/java/com/test/portal4u/NFT/NFTAction/ActionServiceImpl.java
@@ -1,5 +1,23 @@
 package com.test.portal4u.NFT.NFTAction;
 
-public class ActionServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActionServiceImpl implements ActionService {
+
+	@Autowired
+	private ActionDAO dao;
+
+	@Override
+	public List<NFTBoardDTO> list() {
+
+		List<NFTBoardDTO> list = dao.list();
+
+		return list;
+	}
+
 
 }
